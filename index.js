4815bd7e90ad9902a217be4e334969dcc87a6356
msothyn <msothyn@uwo.ca>
2022-11-03 18:51:36 -0400

    Create array for each csv file
    Create array for each csv file

--- index.js
+++ index.js
@@ -1,6 +1,91 @@
 const express = require('express');
 const app = express();
 const port = 3000; 
+
+// Import fs and csv-parse 
+const fs = require("fs");
+const { parse } = require("csv-parse");
+
+// Create 
+const albumArr = [];
+const genreArr = [];
+const artistsArr = [];
+const tracksArr = [];
+
+
+// Parse albums
+fs.createReadStream("./lab3-data/raw_albums.csv")
+  .pipe(
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
+  )
+  .on("data", function (row) {
+    // push the object row into the array
+    data.push(row);
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
+  
+
+  // Parse artists
+  fs.createReadStream("./lab3-data/raw_artists.csv")
+  .pipe(
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
+  )
+  .on("data", function (row) {
+    // push the object row into the array
+    data.push(row);
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
+ 
+
+  // Parse tracks
+  fs.createReadStream("./lab3-data/raw_tracks.csv")
+  .pipe(
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
+  )
+  .on("data", function (row) {
+    // push the object row into the array
+    data.push(row);
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });   
+  
+
+
+  // Parse genres
+  fs.createReadStream("./lab3-data/genres.csv")
+  .pipe(
+    parse({
+      delimiter: ",",
+      columns: true,
+      ltrim: true,
+    })
+  )
+  .on("data", function (row) {
+    // push the object row into the array
+    data.push(row);
+  })
+  .on("error", function (error) {
+    console.log(error.message);
+  });
+
+
  
 // Setup serving front end code 
 app.use('/', express.static('static'));
